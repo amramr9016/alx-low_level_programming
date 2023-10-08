4d29411d4fa019a4121e84a7be9e0263539fc8af
amr <amramr9016@gmail.com>
2023-10-08 13:14:15 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x == 'q' || x == 'e')
+
+			x++;
+			putchar(x);
+
+			x++;
+		
+	}
+
+	putchar('\n');
+
+	return (0);
+}

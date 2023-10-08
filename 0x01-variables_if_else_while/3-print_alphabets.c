65842f5b9301b3b594d43f83af6b638f42d68f9a
amr <amramr9016@gmail.com>
2023-10-08 12:43:42 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char x = 'a';
+	char y = 'A';
+
+	while(x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+
+	while (y <= 'Z')
+	{
+		putchar(y);
+		y++;
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
